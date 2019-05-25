ce30a57fb9bf6d5e0c81d577f907b268a3384f50
xiaoxu5271 <liuchunxun@qq.com>
2019-05-25 15:59:24 +0800

    墙壁开关控制回自动控制4h

--- components/Json_parse/include/Json_parse.h
+++ components/Json_parse/include/Json_parse.h
@@ -26,93 +26,93 @@ esp_err_t creat_object(void);
 #define PROTECT_ON 0X01  //当前有风速等平台保护状态
 #define PROTECT_OFF 0X00 //当前无风速等平台保护状态
 
-#define MAX_AUTO_CTL_TIME (2 * 60) //平台php不发送高角值，切本地计算时间5min
-//#define MAX_WALLKEY_TIME    (4*60*60)  //4h
-#define MAX_WALLKEY_TIME (5 * 60) //墙壁开关控制回自动控制5min
+#define MAX_AUTO_CTL_TIME (2 * 60)     //平台php不发送高角值，切本地计算时间5min
+#define MAX_WALLKEY_TIME (4 * 60 * 60) //4h
+// #define MAX_WALLKEY_TIME (5 * 60) //墙壁开关控制回自动控制5min
 
 struct
 {
-        int8_t mqtt_angle;
-        char mqtt_angle_char[8];
-        int8_t mqtt_height;
-        char mqtt_height_char[8];
-        char mqtt_mode[8];
+    int8_t mqtt_angle;
+    char mqtt_angle_char[8];
+    int8_t mqtt_height;
+    char mqtt_height_char[8];
+    char mqtt_mode[8];
 
-        int8_t mqtt_angle_adj;  //用于手动控制解析改变角度取值-1/+1
-        int8_t mqtt_height_adj; //用于手动控制解析改变高度取值-1/+1
+    int8_t mqtt_angle_adj;  //用于手动控制解析改变角度取值-1/+1
+    int8_t mqtt_height_adj; //用于手动控制解析改变高度取值-1/+1
 
-        int mqtt_last;
+    int mqtt_last;
 
-        int mqtt_sun_condition;
-        char mqtt_sun_condition_char[2];
-        int mqtt_wind_protection;
-        char mqtt_wind_protection_char[2];
-        int mqtt_frost_protection;
-        char mqtt_frost_protection_char[2];
-        int mqtt_fire_alarm;
-        char mqtt_fire_alarm_char[2];
-        char mqtt_stage[8];
-        char mqtt_command_id[32];
-        char mqtt_string[256];
-        char mqtt_Rssi[8];
+    int mqtt_sun_condition;
+    char mqtt_sun_condition_char[2];
+    int mqtt_wind_protection;
+    char mqtt_wind_protection_char[2];
+    int mqtt_frost_protection;
+    char mqtt_frost_protection_char[2];
+    int mqtt_fire_alarm;
+    char mqtt_fire_alarm_char[2];
+    char mqtt_stage[8];
+    char mqtt_command_id[32];
+    char mqtt_string[256];
+    char mqtt_Rssi[8];
 
 } mqtt_json_s;
 
 struct
 {
-        char wifi_ssid[36];
-        char wifi_pwd[36];
+    char wifi_ssid[36];
+    char wifi_pwd[36];
 } wifi_data;
 
 struct
 {
-        int http_angle;
-        int http_height;
-        int http_mode;
-        int http_sun_condition;
-        char http_time[24];
-        char blu_lng[15];
-        char blu_lat[15];
+    int http_angle;
+    int http_height;
+    int http_mode;
+    int http_sun_condition;
+    char http_time[24];
+    char blu_lng[15];
+    char blu_lat[15];
 } http_json_c;
 
 typedef struct
 {
-        char creat_json_b[512];
-        int creat_json_c;
+    char creat_json_b[512];
+    int creat_json_c;
 } creat_json;
 
 typedef struct
 {
-        float lon;
-        float lat;
-        float orientation;
-        int T1_h;
-        int T1_m;
-        int T2_h;
-        int T2_m;
-        int T3_h;
-        int T3_m;
-        int T4_h;
-        int T4_m;
-        uint8_t WallKeyId[4];
-        int8_t Switch;
+    float lon;
+    float lat;
+    float orientation;
+    int T1_h;
+    int T1_m;
+    int T2_h;
+    int T2_m;
+    int T3_h;
+    int T3_m;
+    int T4_h;
+    int T4_m;
+    uint8_t WallKeyId[4];
+    int8_t Switch;
 } object_bluetooth_json;
 
 struct
 {
-        float lon;
-        float lat;
-        float orientation;
-        int T1_h;
-        int T1_m;
-        int T2_h;
-        int T2_m;
-        int T3_h;
-        int T3_m;
-        int T4_h;
-        int T4_m;
-        uint8_t WallKeyId[4];
-        int8_t Switch;
+    float lon;
+    float lat;
+    float orientation;
+    int T1_h;
+    int T1_m;
+    int T2_h;
+    int T2_m;
+    int T3_h;
+    int T3_m;
+    int T4_h;
+    int T4_m;
+    uint8_t WallKeyId[4];
+    int8_t Switch;
 } ob_blu_json;
 
 int read_bluetooth(void);
@@ -121,12 +121,12 @@ void create_http_json(uint8_t post_status, creat_json *pCreat_json);
 
 enum
 {
-        Automatic_mode = 1,       //自动
-        Manual_mode = 0,          //手动
-        Storm_mode = 1,           //风速大强制收帘
-        Not_Storm_mode = 0,       //阳光强，正常控制
-        Frost_protection = 1,     //结霜，强制收帘
-        Not_frost_protection = 0, //未结霜
+    Automatic_mode = 1,       //自动
+    Manual_mode = 0,          //手动
+    Storm_mode = 1,           //风速大强制收帘
+    Not_Storm_mode = 0,       //阳光强，正常控制
+    Frost_protection = 1,     //结霜，强制收帘
+    Not_frost_protection = 0, //未结霜
 };
 
 uint8_t work_status;    //当前工作状态
